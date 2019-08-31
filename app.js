f4a038599d82ad9e98c9725f1e0216846d924665
Arde <c_burns93@yahoo.ca>
2019-08-31 18:26:18 +0200

    mongo atlas setup

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ const graphQLHttp = require('express-graphql');
 //modern javascript called object desctructuring
 //with this i can pull out actuall properties that i need from the package(right hand side)
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
 
+const Event = require('./models/event');
 
 const app = express();
+
+const events = [];
 
 app.use(bodyParser.json());
 
@@ -20,12 +24,27 @@ app.use(bodyParser.json());
 app.use('/graphql',
     graphQLHttp({
         schema: buildSchema(`
-            type RootQuery {
-                events: [String!]!
+            type Event {
+                _id: ID!
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
             }
             
+            input EventInput {
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+            
+            type RootQuery {
+                events: [Event!]!
+            }
+
             type RootMutation {
-                createEvent(name: String): String        
+                createEvent(eventInput: EventInput): Event        
             }
         
             schema {
@@ -36,16 +55,35 @@ app.use('/graphql',
         //resolvers that need to match the schemas by name
         rootValue: {
             events: () => {
-                return ['Romantic Cooking', 'Sailing', 'Techno Dance']
+                return events;
             },
-
+//using the Event model to create new events and save it to db
             createEvent: (args) => {
-                const eventName = args.name;
-                return eventName;
+                const event = new Event({
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price,
+                    date: new Date(args.eventInput.date)
+                });
+               //need to return for grapqhQL to run asynchronously
+               return event.save().then(result => {
+                    console.log(result);
+                   //need to return the event because graphQl createEvent needs a return event
+                    return {...result._doc};
+                }).catch(err =>{
+                    console.log(err);
+                    throw  err;
+                });
             }
         },
         graphiql: true
     })
 );
 
-app.listen(3000);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@alessocluster-g1xq4.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`).then(() => {
+    app.listen(3000);
+}).catch(err =>{
+    console.log(err);
+});
+
+
