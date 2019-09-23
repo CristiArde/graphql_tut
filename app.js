0c59bea37f39b33392c96fa252d72d9d1205f5cc
Arde <c_burns93@yahoo.ca>
2019-09-23 23:44:18 +0200

    separation of files

--- app.js
+++ app.js
@@ -7,167 +7,23 @@ const bodyParser = require('body-parser');
 const graphQLHttp = require('express-graphql');
 //modern javascript called object desctructuring
 //with this i can pull out actuall properties that i need from the package(right hand side)
-const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-
-const Event = require('./models/event');
-const User = require('./models/user');
-
-
 const app = express();
+const qlSchema = require('./graph_ql/schema/index');
+const qlResolvers = require('./graph_ql/resolvers/index');
 
-const events = eventIds => {
-  return Event.find({_id: {$in: eventIds}})
-      .then( events =>{
-          return events.map(event => {
-              return {
-                  ...event._doc,
-                  _id:event.id,
-                  creator: user.bind(this,event.creator)}
-          })
-      })
-      .catch(err => {
-      throw err;
-  })
-};
+
 
 app.use(bodyParser.json());
-
-const user = userId => {
-  return User.findById(userId).then(
-      user => {
-          return {
-              ...user._doc,
-              _id : user.id,
-              createdEvents: events.bind(this, user._doc.createdEvents)
-          };
-      }
-  ).catch(err=> {
-      throw err;
-  })
-};
-
 //explanation
 //https://www.youtube.com/watch?v=LXTyzk2uud0&list=PL55RiY5tL51rG1x02Yyj93iypUuHYXcB_&index=4&t=520s
 //defining graph ql standards
 app.use('/graphql',
     graphQLHttp({
-        schema: buildSchema(`
-            type Event {
-                _id: ID!
-                title: String!
-                description: String!
-                price: Float!
-                date: String!
-                creator: User!
-            }
-            
-            type User {
-                _id: ID!
-                email: String!
-                password: String
-                createdEvents: [Event!]
-            }
-            
-            input UserInput {
-                email: String!
-                password: String!
-            }
-            
-            input EventInput {
-                title: String!
-                description: String!
-                price: Float!
-                date: String!
-            }
-            
-            type RootQuery {
-                events: [Event!]!
-            }
-
-            type RootMutation {
-                createEvent(eventInput: EventInput): Event        
-                createUser(userInput: UserInput): User
-            }
-        
-            schema {
-                query: RootQuery
-                mutation: RootMutation
-            }
-        `),
+        schema: qlSchema,
+        rootValue: qlResolvers,
         //resolvers that need to match the schemas by name
         //find retuns all
-        rootValue: {
-            events: () => {
-                return Event.find()
-                    .then(events => {
-                         return events.map(event => {
-                            return { ...event._doc,
-                                _id: event.id,
-                            creator: user.bind(this, event._doc.creator)};
-                         });
-                    })
-                    .catch( err => {
-                        throw  err;
-                    });
-            },
-//using the Event model to create new events and save it to db
-            createEvent: (args) => {
-                const event = new Event({
-                    title: args.eventInput.title,
-                    description: args.eventInput.description,
-                    price: +args.eventInput.price,
-                    date: new Date(args.eventInput.date),
-                    creator: '5d76b769321ecb123c060fa5'
-                });
-               //need to return for grapqhQL to run asynchronously
-                let createdEvent;
-                return event.save().then(result => {
-                    createdEvent = {
-                        ...result._doc,
-                        _id: result._doc._id.toString(),
-                        creator: user.bind(this, result._doc.creator
-                        )};
-                    return User.findById('5d76b769321ecb123c060fa5')
-                    console.log(result);
-                   //need to return the event because graphQl createEvent needs a return event
-                    return {...result._doc};
-                }).then(user => {
-                    user.createdEvents.push(event);
-                    return user.save();
-                }).then(result => {
-                    return createdEvent;
-                })
-                   .catch(err =>{
-                    console.log(err);
-                    throw  err;
-                });
-            },
-
-            createUser: (args) => {
-               return User.findOne({
-                   email: args.userInput.email
-                }).then(user => {
-                    if(user){
-                        throw new Error('User already exists')
-                    }
-                    return bcrypt
-                        .hash(args.userInput.password, 12)
-                }).then(hashPass => {
-                        const user = new User({
-                            email: args.userInput.email,
-                            password: hashPass
-                        });
-                        return user.save();
-                    }).then(result => {
-                        return {...result._doc, password:null, _id: result.id}
-                   })
-                    .catch(err => {
-                    throw  err;
-                });
-            }
-        },
         graphiql: true
     })
 );
@@ -178,3 +34,4 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PA
     console.log(err);
 });
 
+part 7 min 21
